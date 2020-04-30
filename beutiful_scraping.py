78a8c815a74b228d7a2fb10a4a3a327933019a51
ramo <ramo@ramo.local>
2020-04-30 23:44:48 +0900

    def切り分けテスト

--- beutiful_scraping.py
+++ beutiful_scraping.py
@@ -1,25 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 
+def get_target_url(url):
+    url_list = []
+    for count in range(10):
+        res = requests.get(url)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        targets = soup.find(id="list01")
+        target_url = targets.select("h3 > a")
+        for tmp in target_url:
+            url_list.append(tmp['href'])
+    return url_list
 
 
-url_list = []
+a = 'https://auctions.yahoo.co.jp/seller/tomokimi_777'
+test = get_target_url(a)
+
+print(test)
 
 
-res = requests.get('https://auctions.yahoo.co.jp/seller/tomokimi_777')
-soup = BeautifulSoup(res.text, 'html.parser')
+# res = requests.get('https://auctions.yahoo.co.jp/seller/tomokimi_777')
+# soup = BeautifulSoup(res.text, 'html.parser')
 
-test = soup.find(class_="next")
-a = test.find('a')
-print(a['href'])
+# test = soup.find(class_="next")
+# a = test.find('a')
+# print(a['href'])
 
-# print(url_list)
 
-# for count in range(10):
-#     res = requests.get('https://auctions.yahoo.co.jp/seller/tomokimi_777')
-#     soup = BeautifulSoup(res.text, 'html.parser')
-#     targets = soup.find(id="list01")
-#     target_url = targets.select("h3 > a")
-#     for tmp in target_url:
-#         url_list.append(tmp['href'])
 
