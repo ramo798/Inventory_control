4e1611989392f2fc4a078dc48a41a2714e534c32
ramo <ramo@ramo.local>
2020-04-30 23:28:44 +0900

    url取得テスト

--- beutiful_scraping.py
+++ beutiful_scraping.py
@@ -1,13 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 
-res = requests.get('https://auctions.yahoo.co.jp/seller/tomokimi_777')
 
+
+url_list = []
+
+
+res = requests.get('https://auctions.yahoo.co.jp/seller/tomokimi_777')
 soup = BeautifulSoup(res.text, 'html.parser')
 
-targets = soup.find(id="list01")
+test = soup.find(class_="next")
+a = test.find('a')
+print(a['href'])
 
-target_url = targets.select("h3 > a")
+# print(url_list)
 
-for tmp in target_url:
-    print(tmp.text)
+# for count in range(10):
+#     res = requests.get('https://auctions.yahoo.co.jp/seller/tomokimi_777')
+#     soup = BeautifulSoup(res.text, 'html.parser')
+#     targets = soup.find(id="list01")
+#     target_url = targets.select("h3 > a")
+#     for tmp in target_url:
+#         url_list.append(tmp['href'])
+
