630670f4ec84ac5a618b6eb611749ae011fcfdf6
ramo <ramo@ramo.local>
2020-05-01 00:09:22 +0900

    商品リストのURL取得def

--- beutiful_scraping.py
+++ beutiful_scraping.py
@@ -1,22 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_target_url(url):
-    url_list = []
-    for count in range(10):
+# def get_target_url(access_url):
+#     url_list = []
+#     url = access_url
+#     for count in range(1):
+#         res = requests.get(url)
+#         soup = BeautifulSoup(res.text, 'html.parser')
+#         targets = soup.find(id="list01")
+#         target_url = targets.select("h3 > a")
+#         for tmp in target_url:
+#             url_list.append(tmp['href'])
+#     return url_list
+
+def get_list(access_url):
+    page_list = []
+    url = access_url
+    for tmp in range(10):
         res = requests.get(url)
         soup = BeautifulSoup(res.text, 'html.parser')
-        targets = soup.find(id="list01")
-        target_url = targets.select("h3 > a")
-        for tmp in target_url:
-            url_list.append(tmp['href'])
-    return url_list
+        try:
+            next_button = soup.find(class_="next")
+            next_button_url = next_button.find('a')
+            page_list.append(next_button_url['href'])
+            url = next_button_url['href']
+        except TypeError as e:
+            pass
 
+        
+    return page_list
 
-a = 'https://auctions.yahoo.co.jp/seller/tomokimi_777'
-test = get_target_url(a)
-
+url = 'https://auctions.yahoo.co.jp/seller/tomokimi_777'
+test = get_list(url)
 print(test)
+
+# a = 'https://auctions.yahoo.co.jp/seller/tomokimi_777'
+# test = get_target_url(a)
+# print(len(test))
 
 
 # res = requests.get('https://auctions.yahoo.co.jp/seller/tomokimi_777')
