5c00334fce9a2fb3ad6df3ee9d9f2d2ed17d9ced
ramo <knht31i@gmail.com>
2020-05-06 00:36:21 +0900

    csvのデータをdbに登録完了

--- dynamo_get.py
+++ dynamo_get.py
@@ -2,11 +2,13 @@ import boto3
 from boto3.dynamodb.conditions import Key
 
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('inventry-1')
+table = dynamodb.Table('inventry')
 
-response = table.get_item(
-    Key={
-        'model_number': "UC78"
-    }
-)
-print(response['Item'])
+# response = table.get_item(
+#     Key={
+#         'model_number': "UC78"
+#     }
+# )
+response = table.scan()
+print(response['Items'])
+print(len(response['Items']))
--- dynamo_put.py
+++ dynamo_put.py
@@ -1,13 +1,26 @@
 import boto3
 from boto3.dynamodb.conditions import Key
+import csv
 
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('inventry-1')
+table = dynamodb.Table('inventry')
 
-table.put_item(
-    Item={
-        'model_number': "UC78",
-        'date': "2020-05-04",
-        'price': "19,800",
-    }
-)
+
+
+with open('tomokimi_777.csv') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print(row)
+        table.put_item(
+            Item={
+                'model_number': row[0],
+                'title':row[1],
+                'price':row[2],
+                'kata':row[3],
+                'mune':row[4],
+                'take':row[5],
+                'sode':row[6],
+                'yahuoku_last_check_date': row[7],
+            }
+        )
+        
