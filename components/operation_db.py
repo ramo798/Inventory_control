d8aff9eadf6f4c683b07121a77c171a17925152e
ramo <knht31i@gmail.com>
2020-05-11 10:22:41 +0900

    テーブル上のデータの存在確認

--- components/operation_db.py
+++ components/operation_db.py
@@ -6,11 +6,28 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('inventry')
 today = str(datetime.date.today())
 
-# def check_existence(dict):
+# テーブルに存在しているデータかの判定
+def check_existence(dict):
+    model_number = dict['measuring']['id']
+    # print(model_number)
+
+    response = table.get_item(
+    Key={
+        'model_number': model_number
+    }
+    )
+
+    if 'Item' in response:
+        # print(response['Item'])
+        return True
+    else:
+        return False
 
 
 
 if __name__ == '__main__':
 
-    testdata = {'title': '☆DONNA KARAN☆チュール付きキャミソールM', 'price': '5,800', 'measuring': {'id': 'DUC62', 'kata': '', 'take': '123', 'bast': '38', 'sode': ''}, 'url': 'https://page.auctions.yahoo.co.jp/jp/auction/p763454105'}
-    print(testdata['title'])
+    testdata = {'title': '☆DONNA KARAN☆チュール付きキャミソールM', 'price': '5,800', 'measuring': {'id': 'UC78', 'kata': '', 'take': '123', 'bast': '38', 'sode': ''}, 'url': 'https://page.auctions.yahoo.co.jp/jp/auction/p763454105'}
+    # print(testdata['title'])
+    if check_existence(testdata):
+        print(True)
