0e03ffc8b0822e46a458978e3a58dc2b6983e9ea
ramo <knht31i@gmail.com>
2020-05-11 12:32:26 +0900

    データの更新の機能の実装

--- components/operation_db.py
+++ components/operation_db.py
@@ -31,7 +31,7 @@ def remove_blank(value):
 
 # データベースに値を登録
 def put_item(dict):
-
+    
     table.put_item(
         Item={
             'model_number': remove_blank(dict['measuring']['id']),
@@ -42,26 +42,48 @@ def put_item(dict):
             'take':remove_blank(dict['measuring']['take']),
             'sode':remove_blank(dict['measuring']['sode']),
             'yahuoku_last_check_date': today,
-            'url':remove_blank(dict['url'])
+            'item_url':remove_blank(dict['url'])
         }
     )
-    
+
+def update_item(dict):
+    model_number = dict['measuring']['id']
+
+    response = table.update_item(
+    Key={
+        'model_number': model_number
+    },
+    UpdateExpression="set title = :title, price = :price, kata = :kata, mune = :mune,take = :take, sode = :sode, yahuoku_last_check_date = :today, item_url = :url",
+    ExpressionAttributeValues={
+        ':title':remove_blank(dict['title']),
+        ':price':remove_blank(dict['price']),
+        ':kata':remove_blank(dict['measuring']['kata']),
+        ':mune':remove_blank(dict['measuring']['bast']),
+        ':take':remove_blank(dict['measuring']['take']),
+        ':sode':remove_blank(dict['measuring']['sode']),
+        ':today': today,
+        ':url':remove_blank(dict['url'])
+    },
+    ReturnValues="UPDATED_NEW"
+)
 
 if __name__ == '__main__':
 
-    testdata = {'title': '☆DONNA KARAN☆チュール付きキャミソールM',
-     'price': '5,800',
-     'measuring': {
-         'id': 'UC781',
-         'kata': '', 
-         'take': '123', 
-         'bast': '38', 
-         'sode': ''
-         },
-     'url': 'https://page.auctions.yahoo.co.jp/jp/auction/p763454105'
+    testdata = {
+        'title': '☆DONNA KARAN☆チュール付きキャミソールM',
+        'price': '5,8000',
+        'measuring': {
+            'id': 'UC7811',
+            'kata': '', 
+            'take': '123', 
+            'bast': '381', 
+            'sode': ''
+        },
+        'url': 'https://page.auctions.yahoo.co.jp/jp/auction/p763454105'
      }
      
-    put_item(testdata)
+    # put_item(testdata)
+    update_item(testdata)
 
     # print(testdata['title'])
     # if check_existence(testdata):
