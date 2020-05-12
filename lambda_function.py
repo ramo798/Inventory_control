b2f11e79e852bf37e8c482d76522a817aeee7692
ramo <knht31i@gmail.com>
2020-05-12 17:33:46 +0900

    lambda用のコードを作成

---
+++ lambda_function.py
@@ -0,0 +1,18 @@
+import json
+from components import get_items as gi
+from components import operation_db as op
+
+def lambda_handler(event, context):
+    # データベースの更新
+    users = ['tomokimi_777','merci_dsyl']
+    for username in users:
+        items = gi.get_items(username)
+        print('GET ITEM FINISH. FROM ',username)
+
+        for item in items:
+            op.operation(item,username)
+
+    # sold_out判定用のバッチ処理
+    op.sold_out_checker()
+    
+    return 'suc'
