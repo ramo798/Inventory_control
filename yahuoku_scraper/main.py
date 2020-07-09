9df8364bfd094ad8f6248f703b60df95ba0dfe13
ramo <knht31i@gmail.com>
2020-07-09 20:16:33 +0900

    スクレイパーの完成

--- yahuoku_scraper/main.py
+++ yahuoku_scraper/main.py
@@ -1,5 +1,12 @@
 import scraping as sc
 
 if __name__ == '__main__':
-    users = ["younghoho_1121"]
-    sc.get_items(users[0])
+    users = ["younghoho_1121","tomokimi_777"]
+    # users = ["merci_dsyl"]
+    for username in users:
+        items = sc.get_items(username)
+        print('GET ITEM FINISH. FROM ',username)
+
+        # for item in items:
+        #     print(item)
+        #     op.operation(item,username)
