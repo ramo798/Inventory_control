2dc146d0686ed6342195e4dc35f7cc097f3afac7
ramo798 <knht31i@gmail.com>
2021-04-22 02:24:47 +0900

    リファクタリング

--- test.py
+++ test.py
@@ -1,29 +1,15 @@
-from components import get_items as gi
+import get_items as gi
 import csv
 import pandas as pd
 
 if __name__ == '__main__':
     res_list = []
-    # データベースの更新
     users = ["younghoho_1121", "tomokimi_777"]
-    # users = ["merci_dsyl"]
+
     for username in users:
         items = gi.get_items(username)
         print('GET ITEM FINISH. FROM ', username)
 
-        for item in items:
-            print(item)
-            res_list.append(item)
-
-            # writer.writerow(item)
-            # op.operation(item, username)
-
-    df = pd.io.json.json_normalize(res_list)
-    print(df)
-    df.to_csv('dogs.csv')
-
-    # sold_out判定用のバッチ処理
-    # op.sold_out_checker()
-
-    # items = ge.generate()
-    # up.upload_csv(items)
+        # for item in items:
+        #     print(item)
+        #     res_list.append(item)
